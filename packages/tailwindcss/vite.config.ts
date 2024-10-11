bfa172e5aa45f32b81dc4a99736e60d220afc04b
corb3nik <ian@atvh.dev>
2024-10-11 11:32:45 -0400

    Add tailwindcss package

--- packages/tailwindcss/vite.config.ts
+++ packages/tailwindcss/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,16 +8,6 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.ts'),
       formats: ["es"]
     },
-  },
-  plugins: [
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'src/tokens.css',
-          dest: '.'
-        }
-      ]
-    })
-  ],
+  }
 })
 
